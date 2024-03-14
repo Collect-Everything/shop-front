5954f8e66a162f9669d1a2f16f44c93b35a4dd86
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2024-03-14 09:30:27 +0100

    feat: finish home page

---
+++ components/catalog/ItemCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="flex flex-col items-start space-y-1"
+    @click="$router.push(`/products/${product.id}`)"
+  >
+    <img
+      :src="product.image"
+      alt="image"
+      class="border border-neutral-200 rounded-md"
+    />
+    <span class="font-bold title">{{ product.name }}</span>
+    <span class="text-neutral-700 title">{{ product.price }} €</span>
+  </div>
+</template>
+
+<script lange="ts">
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col text-center space-y-6">
+  <div class="flex flex-col items-center text-center space-y-6 pb-4">
     <div
       class="bg-black w-full max-h-96 relative flex items-center justify-center text-center"
     >
@@ -27,12 +27,36 @@
       </div>
     </div>
 
-    <span class="text-3xl">Nos produits locaux et de saison</span>
+    <div class="flex flex-col items-center text-center space-y-6 px-4">
+      <span class="text-3xl">Nos produits locaux et de saison</span>
 
-    <span class="text-neutral-600 px-4">
-      Chez la cagette locale, nous sommes fiers de travailler avec des
-      producteurs locaux pour vous offrir des produits frais et de saison.
-    </span>
+      <span class="text-neutral-600">
+        Chez la cagette locale, nous sommes fiers de travailler avec des
+        producteurs locaux pour vous offrir des produits frais et de saison.
+      </span>
+
+      <div class="grid grid-cols-2 gap-4">
+        <CatalogItemCard v-for="item in items" :key="item.id" :product="item" />
+      </div>
+
+      <button
+        :style="`background-color: ${color}`"
+        class="btn w-fit"
+        @click="$router.push('/catalog')"
+      >
+        Voir tous les produits
+      </button>
+
+      <div
+        v-for="advantage in advantages"
+        :key="advantage.id"
+        class="flex flex-col justify-between space-y-4 p-4 bg-gray-200 min-h-28 w-full"
+      >
+        <!-- <fa-icon v-if="advantage.icon" :icon="['fas', advantage.icon]" /> -->
+        <span class="font-semibold">{{ advantage.title }}</span>
+        <span>{{ advantage.description }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -46,6 +70,64 @@ export default {
       buttonText: 'Découvrez notre catalogue',
       backgroundImage: 'https://source.unsplash.com/1600x900/?grocery',
       color: '#3C7E44',
+      items: [
+        {
+          id: 1,
+          name: 'Pommes de terre',
+          price: 2.5,
+          image: 'https://source.unsplash.com/1600x900/?potato',
+        },
+        {
+          id: 2,
+          name: 'Tomates',
+          price: 3.5,
+          image: 'https://source.unsplash.com/1600x900/?tomato',
+        },
+        {
+          id: 3,
+          name: 'Carottes',
+          price: 1.5,
+          image: 'https://source.unsplash.com/1600x900/?carrot',
+        },
+        {
+          id: 4,
+          name: 'Oignons',
+          price: 2,
+          image: 'https://source.unsplash.com/1600x900/?onion',
+        },
+        {
+          id: 5,
+          name: 'Salade',
+          price: 2.5,
+          image: 'https://source.unsplash.com/1600x900/?lettuce',
+        },
+        {
+          id: 6,
+          name: 'Pommes',
+          price: 3,
+          image: 'https://source.unsplash.com/1600x900/?apple',
+        },
+      ],
+      advantages: [
+        {
+          id: 1,
+          title: 'Produits locaux',
+          description:
+            'Nous travaillons avec des producteurs locaux pour vous offrir des produits frais et de qualité',
+        },
+        {
+          id: 2,
+          title: 'Service rapide',
+          description:
+            'Commandez en ligne et récupérez votre panier en magasin en un temps record',
+        },
+        {
+          id: 3,
+          title: 'Produits de saison',
+          description:
+            'Nous vous proposons des produits de saison pour une consommation plus responsable',
+        },
+      ],
     }
   },
 }
