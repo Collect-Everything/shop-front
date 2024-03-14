4379b43667964bcb8de0e8a60af5a84ba6d181cd
sakerlucas <sakerlucas@gmail.com>
2024-03-14 08:51:46 +0100

    feat: init catalog, contact and login pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,32 @@
   <div class="text-black h-screen w-full">
     <div
       v-if="screenWidth < 768"
-      class="flex items-center justify-between border-b border-gray-300 w-full p-4"
+      class="flex items-center justify-between w-full p-4"
     >
-      <fa-icon
-        :icon="['fas', 'bars']"
-        class="text-2xl"
-        @click="showNav = !showNav"
-      />
+      <div class="space-x-6">
+        <fa-icon
+          :icon="['fas', 'bars']"
+          class="text-2xl"
+          @click="showNav = !showNav"
+        />
+        <span class="font-semibold text-xl">{{ shopName }}</span>
+      </div>
+      <div class="space-x-6">
+        <fa-icon :icon="['fas', 'search']" class="text-2xl text-neutral-500" />
+        <fa-icon
+          :icon="['fas', 'bag-shopping']"
+          class="text-2xl text-neutral-500"
+        />
+      </div>
     </div>
     <div
       v-else
       class="flex items-center justify-between border-b border-gray-300 w-full px-6 py-4"
     >
       <div class="w-1/2 flex items-center justify-center space-x-6">
-        <span class="font-semibold text-neutral-900">{{ shopName }}</span>
+        <NuxtLink to="/" class="text-neutral-500">
+          <span>{{ shopName }}</span>
+        </NuxtLink>
 
         <NuxtLink to="/catalog" class="text-neutral-500">
           <span>
@@ -44,15 +56,51 @@
     </div>
     <div
       v-else
-      class="flex flex-col items-center justify-center space-y-6 pt-6 divide-y divide-gray-300 w-full"
+      class="flex flex-col items-start justify-center space-y-8 p-6 w-full"
     >
+      <span class="text-2xl font-semibold pb-6">{{ shopName }}</span>
       <NuxtLink
         to="/"
         class="flex items-center justify-center space-x-2"
         @click="showNav = false"
       >
-        <fa-icon :icon="['fas', 'home']" />
-        <span>{{ $t('general.home') }}</span>
+        <fa-icon :icon="['fas', 'home']" class="text-neutral-500 text-xl" />
+        <span class="text-neutral-900 text-xl">Accueil</span>
+      </NuxtLink>
+      <NuxtLink
+        to="/catalog"
+        class="flex items-center justify-center space-x-2"
+        @click="showNav = false"
+      >
+        <fa-icon
+          :icon="['fas', 'book-open']"
+          class="text-neutral-500 text-xl"
+        />
+        <span class="text-neutral-900 text-xl">
+          {{ $t('general.catalog') }}
+        </span>
+      </NuxtLink>
+      <NuxtLink
+        to="/contact"
+        class="flex items-center justify-center space-x-2"
+        @click="showNav = false"
+      >
+        <fa-icon
+          :icon="['fas', 'paper-plane']"
+          class="text-neutral-500 text-xl"
+        />
+        <span class="text-neutral-900 text-xl"> Contact </span>
+      </NuxtLink>
+      <NuxtLink
+        to="/contact"
+        class="flex items-center justify-center space-x-2"
+        @click="showNav = false"
+      >
+        <fa-icon
+          :icon="['fas', 'arrow-right-to-bracket']"
+          class="text-neutral-500 text-xl"
+        />
+        <span class="text-neutral-900 text-xl">Connexion / Inscription</span>
       </NuxtLink>
     </div>
   </div>
